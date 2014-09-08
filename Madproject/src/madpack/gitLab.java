d400be662990a5bd169de3fab21c7464ba60b5ea
Madb0t <madskillzbot@gmail.com>
2014-09-08 17:56:21 +0600

    Mad

---
+++ Madproject/src/madpack/gitLab.java
@@ -0,0 +1,19 @@
+package madpack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: student
+ * Date: 08.09.14
+ * Time: 15:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class gitLab {
+    public static void helloMAD(){
+        System.out.println("hello world from Mad");
+    }
+
+    public static void main (String[] args){
+        new gitLab();
+        helloMAD();
+    }
+}
